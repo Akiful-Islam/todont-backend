72d19df051a824358a013349795f9cb07c6a8f80
Arian Islam <arian.islam024@gmail.com>
2024-04-04 11:58:21 +0600

    Update entity with validation

--- src/main/java/com/ids/todont/entity/Todo.java
+++ src/main/java/com/ids/todont/entity/Todo.java
@@ -1,23 +1,35 @@
 package com.ids.todont.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+import java.util.Date;
+
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 @Entity
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NonNull
+    @NotEmpty(message = "Title is required")
     private String title;
     private String description;
-    private boolean completed;
+    @NonNull
+    @NotEmpty(message = "Due date is required")
+    private Date dueDate;
+    @NonNull
+    @NotNull
+    @Enumerated(EnumType.STRING)
     private Priority priority;
+    @NotNull
+    @NotEmpty
+    private boolean completed;
 }
