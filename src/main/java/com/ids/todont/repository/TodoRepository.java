ae3e9ca6d2ed7545625ac4577931170405cbaae2
Arian Islam <arian.islam024@gmail.com>
2024-04-04 13:03:56 +0600

    Add filtering method in repository

--- src/main/java/com/ids/todont/repository/TodoRepository.java
+++ src/main/java/com/ids/todont/repository/TodoRepository.java
@@ -1,9 +1,14 @@
 package com.ids.todont.repository;
 
+import com.ids.todont.entity.Priority;
 import com.ids.todont.entity.Todo;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long>{
+    Page<Todo> findByCompletedOrPriorityOrTitleOrDescription(Boolean completed, Priority priority, String title, String description, Pageable pageable);
 }
