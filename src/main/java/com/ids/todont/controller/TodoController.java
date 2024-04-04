c727ef80cef586b6c2458e852913f75777d8edc1
Arian Islam <arian.islam024@gmail.com>
2024-04-04 13:03:41 +0600

    Add REST endpoints

--- src/main/java/com/ids/todont/controller/TodoController.java
+++ src/main/java/com/ids/todont/controller/TodoController.java
@@ -1,8 +1,11 @@
 package com.ids.todont.controller;
 
+import com.ids.todont.dto.TodoDto;
 import com.ids.todont.service.TodoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/todos")
@@ -11,5 +14,31 @@ public class TodoController {
 
     public TodoController(TodoService todoService) {
         this.todoService = todoService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<TodoDto>> findAllTodos(Pageable pageable){
+        return ResponseEntity.ok(todoService.findAll(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TodoDto> findTodoById(@PathVariable Long id){
+        return ResponseEntity.ok(todoService.findTodoById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<TodoDto> save(@RequestBody TodoDto todoDto){
+        return ResponseEntity.ok(todoService.save(todoDto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TodoDto> update(@PathVariable Long id, @RequestBody TodoDto todoDto){
+        return ResponseEntity.ok(todoService.update(id, todoDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        todoService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
