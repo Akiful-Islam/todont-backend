f169480b4b07ab92765340da95f980811ee94506
Arian Islam <arian.islam024@gmail.com>
2024-04-15 11:45:34 +0600

    update enum and add initial dummy data

--- src/main/java/com/ids/todont/entity/Priority.java
+++ src/main/java/com/ids/todont/entity/Priority.java
@@ -1,5 +1,5 @@
 package com.ids.todont.entity;
 
 public enum Priority {
-    LOWEST, LOW, MEDIUM, HIGH, HIGHEST
+    LOWEST, LOW, NORMAL, HIGH, HIGHEST
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO todo(title, description, due_date, priority, completed)
+VALUES ('Learn Spring', 'Learn Spring Framework', '2024-03-31', 'HIGH', false),
+       ('Learn Spring Boot', 'Learn Spring Boot', '2024-03-31', 'HIGHEST', false),
+       ('Learn Spring Data JPA', 'Learn Spring Data JPA', '2024-03-31', 'LOW', false),
+       ('Learn Spring Security', 'Learn Spring Security', '2024-03-31', 'NORMAL', false),
+       ('Learn Spring Cloud', 'Learn Spring Cloud', '2024-03-31', 'LOWEST', false);
